7dc22bf277a3427bb103637d45dbcc21cf913b19
Goran Matić <goran1matic@gmail.com>
2024-05-09 01:45:17 +0200

    Create Main.py
    Initial version

---
+++ ETL_Project/Main.py
@@ -0,0 +1,17 @@
+import FileCheck.py
+			
+# class for starting process		
+class Main:
+		
+	def __init__( self  ):
+ 		#logovanje inicijalizacije fajla
+		pass		
+		
+	def main( self ):
+		#logovanje start
+		pass
+
+
+fc = FileCheck()
+fc.mainLoop()
+
