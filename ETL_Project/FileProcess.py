329e083d04b67e6945f2cff74d149b1ec30b180f
Goran Matić <goran1matic@gmail.com>
2024-05-09 01:44:32 +0200

    Create FileProcess.py
    Initial version

---
+++ ETL_Project/FileProcess.py
@@ -0,0 +1,49 @@
+from pathlib import Path		
+
+#class for processing of one file
+class FileProcess:
+	
+	def __init__( self , path : str, fileName : str ):
+ 		#logovanje inicijalizacije fajla
+		self.fileName : str = fileName
+		self.path : str = path		
+			
+	def processFile( self ):
+		#logovanje da je pocela obrada fajla		
+		print( ' processFile - Start ' )
+		
+		if self.fileName.find('.csv') >= 0:
+
+			self.processFile_csv()	
+			pass
+
+		elif self.fileName.find('.xls') >= 0:
+
+			self.processFile_xls()	
+			pass
+
+		else:
+			#nepoznat format
+			#log
+			pass  
+
+		self.moveProcessedFile()
+
+	# Method to process csv file
+	def processFile_csv( self ):
+		print( 'processFile_csv' )
+		pass
+	
+	#Method to process xls file
+	def processFile_xls( self ):
+		print( 'processFile_xls' )	
+		pass
+
+	def moveProcessedFile( self ):
+		print( 'moveProcessedFile' )	
+		self.processedFilePath : str = "/home/porti/Documents/ROOT/IT/Python/ETL/Files_Repo/Processed_Files"
+		try:
+			Path( self.path + '/' + self.fileName ).rename( self.processedFilePath + '/' +self.fileName )
+			print( 'Fajl je prenet' )
+		except Exception as e:
+			print( e )
