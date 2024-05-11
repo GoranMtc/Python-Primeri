2a426960b2155110bee8a740190840f7f9623410
Goran Matić <goran1matic@gmail.com>
2024-05-11 17:51:12 +0200

    initial version

---
+++ ETL_Project/connect.py
@@ -0,0 +1,30 @@
+import psycopg2
+from config import load_config
+
+class db_connection:
+	
+	def connect( self, config ):
+		#Connect to the PostgreSQL database server
+		try:
+			self.conn= psycopg2.connect( **config )
+
+		except (psycopg2.DatabaseError, Exception) as error:
+			print(error)
+		else:
+			print('Connected to the PostgreSQL server.')
+						
+	def create_connection( self ):
+		try:
+			config = load_config()
+			self.connect( config )
+
+		except Exception as e:
+			print( e )
+
+	def get_connection( self ):
+		return self.conn
+
+if __name__ == '__main__':
+	dbc = db_connection()
+	dbc.create_connection()
+
---
+++ ETL_Project/connect_pg.py
@@ -0,0 +1,9 @@
+from pg import DB
+
+db = DB( dbname='my_test', 
+         host='localhost', 
+         port=5432,
+         user='postgres', 
+         passwd='password')
+         
+         
