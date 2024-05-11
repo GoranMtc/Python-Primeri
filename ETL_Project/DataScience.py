c1111d9cbba638db8d913082e0dc21f85eeca22f
Goran Matić <goran1matic@gmail.com>
2024-05-11 18:42:50 +0200

    Add files via upload

--- ETL_Project/DataScience.py
+++ ETL_Project/DataScience.py
@@ -22,6 +22,22 @@ class DataScience():
 
 		print( self.dfColumnTypes )
 		
+		#obradi kolone kojima fale podaci
+		#dopuni kolone kojima fale podace
+		#interpolacija podataka
+		#da se zapise koji su slogovi uklonjeni
+		#da se kreira index kolona
+		#da se prikazu grafici
+		#da se upise u neku tabelu koji su fajlovi bili procesuirani, koliko je to trajalo
+		#da se u postgresu uradi procedura koja prenosi inkrementalno podatke u drugu semu
+		#da se u proceduri uloguju potencijalne greske
+		#da se uradi rekurzivni upit ... za nesto
+		#da se kroz fajl proslede imena user-a i privilegija za user-e i dinamicki izvrti
+		#da se uradi automatsko razmotavanje baze
+		#da se u toku obrade prikazuje vrteci znak [ / -- | \ -- ...]
+        #da posalje mail kada se obrada zavrsi
+		
+		
 	#	
 		
 	
